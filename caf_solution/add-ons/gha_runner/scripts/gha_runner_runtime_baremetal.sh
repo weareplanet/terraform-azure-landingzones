467172ee959650df357ac2aca338f4b4b9520a2e
Chris Sievering <chris.sievering@planetpayment.com>
2023-10-02 13:19:17 -0400

    new fix

--- caf_solution/add-ons/gha_runner/scripts/gha_runner_runtime_baremetal.sh
+++ caf_solution/add-ons/gha_runner/scripts/gha_runner_runtime_baremetal.sh
@@ -34,7 +34,7 @@ apt-get install -y --no-install-recommends \
   jq
 
 mkdir -m 0755 -p /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg |gpg --no-tty --dearmor -o /etc/apt/keyrings/docker.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg |gpg --batch --yes --dearmor -o /etc/apt/keyrings/docker.gpg
 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
